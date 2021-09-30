f4606ca55a5e22ec3c53d42ba4e50994bdf93324
yasutaka-imaeda <90546310+yasutaka-imaeda@users.noreply.github.com>
2021-09-30 19:21:15 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -4,14 +4,27 @@
 
   document.getElementById('input-button').addEventListener('click', () => {
     const li = document.querySelector('li');
+    var gbtn = document.createElement('button');
+    var bbtn = document.createElement('button');
+    gbtn.innerHTML = 'good';
+    gbtn.id = 'good-btn';
+    bbtn.innerHTML = 'bad';
+    bbtn.id = 'bad-btn';
 
     const text = document.querySelector('input');
 
     li.textContent = text.value;
     document.querySelector('ul').appendChild(li);
+    document.querySelector('ul').appendChild(gbtn);
+    document.querySelector('ul').appendChild(bbtn);
 
     text.value = '';
     text.focus();
+  });
+
+  document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log('submit');
   });
 
 
